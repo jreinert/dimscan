044c2db959be7b6fd32cf57b01e68a4d696a8053
Joakim Reinert <mail@jreinert.com>
2015-03-23 10:30:02 +0100

    Revert "Recycling http connections for faster scans"
    This reverts commit e8de6962133a7f9e887da42d1b971f6510a9bc0b.
    For faster scans, use threads

--- lib/dimscan.rb
+++ lib/dimscan.rb
@@ -1,12 +1,6 @@
 require 'dimscan/version'
 require 'dimscan/jpeg_scanner'
 require 'dimscan/png_scanner'
-require 'dimscan/http_byte_enumerator'
 
 module Dimscan
-  class << self
-    def close_connections
-      HTTPByteEnumerator.close_connections
-    end
-  end
 end
--- spec/dimscan/http_byte_enumerator_spec.rb
+++ spec/dimscan/http_byte_enumerator_spec.rb
@@ -2,93 +2,10 @@ require 'uri'
 require 'net/http'
 require 'spec_helper'
 require 'dimscan/http_byte_enumerator'
-require 'pry'
 
 # HTTPByteEnumerator specs
 module Dimscan
   describe HTTPByteEnumerator do
-    let(:response_stub) { double('response') }
-    let(:http_stub) do
-      result = double('http')
-      allow(result).to receive(:request).with(:get_request).and_yield(
-        response_stub
-      )
-      result
-    end
-
-    let(:net_http_stub) do
-      http_stub = self.http_stub
-      Module.new do
-        define_method(:start) do |*|
-          http_stub
-        end
-        module_function :start
-      end
-    end
-
-    let(:net_http_get_stub) { double('Net::HTTP::Get', new: :get_request) }
-
-    before(:each) do
-      stub_const('Net::HTTP', net_http_stub)
-      stub_const('Net::HTTP::Get', net_http_get_stub)
-    end
-
-    describe '.open_connection' do
-      it 'requires host, port and use_ssl arguments' do
-        expect { HTTPByteEnumerator.open_connection }.to raise_error(
-          ArgumentError
-        )
-        expect { HTTPByteEnumerator.open_connection('foo') }.to raise_error(
-          ArgumentError
-        )
-        expect do
-          HTTPByteEnumerator.open_connection('foo', 'bar')
-        end.to raise_error(ArgumentError)
-        HTTPByteEnumerator.open_connection('foo', 'bar', false)
-      end
-
-      it 'opens a new http connection' do
-        expect(net_http_stub).to receive(:start).with(
-          'https://valid/https/url',
-          443,
-          use_ssl: true
-        )
-        HTTPByteEnumerator.open_connection(
-          'https://valid/https/url',
-          443,
-          true
-        )
-      end
-
-      it 'reuses old connections' do
-        expect(net_http_stub).to receive(:start).once.and_return(:baz)
-        HTTPByteEnumerator.open_connection('foo', 'bar', 'baz')
-        result = HTTPByteEnumerator.open_connection('foo', 'bar', 'baz')
-        expect(result).to eq(:baz)
-      end
-    end
-
-    describe '.close_connections' do
-      it 'calls finish on all connections' do
-        connection = double('http')
-        other_connection = double('http')
-        HTTPByteEnumerator.instance_variable_set(
-          :@connections,
-          foo: {
-            bar: {
-              baz: connection
-            },
-            foobar: {
-              foo: other_connection
-            }
-          }
-        )
-        expect(connection).to receive(:finish)
-        expect(other_connection).to receive(:finish)
-        HTTPByteEnumerator.close_connections
-      end
-    end
-
     describe '.new' do
       it 'requires a http/https url' do
         expect { HTTPByteEnumerator.new }.to raise_error(ArgumentError)
@@ -123,12 +40,34 @@ module Dimscan
         )
         response
       end
+      let(:net_http_get_stub) { double('Net::HTTP::Get', new: :get_request) }
+      let(:http_stub) do
+        result = double('http')
+        allow(result).to receive(:request).with(:get_request).and_yield(
+          response_stub
+        )
+        result
+      end
+      let(:net_http_stub) do
+        Module.new do
+          def start(*)
+            yield http_stub
+          end
+          module_function :start
+        end
+      end
+
+      before(:each) do
+        stub_const('Net::HTTP', net_http_stub)
+        stub_const('Net::HTTP::Get', net_http_get_stub)
+        allow(Net::HTTP).to receive(:start).and_yield(http_stub)
+      end
 
       it 'opens a HTTP connection' do
-        expect(HTTPByteEnumerator).to receive(:open_connection).with(
-          uri.host, uri.port,  false
-        ).and_return(http_stub)
         enumerator.each { |_| }
+        expect(Net::HTTP).to have_received(:start).with(
+          uri.host, uri.port, use_ssl: false
+        )
       end
 
       it 'sends a get request to the given url' do
@@ -138,12 +77,10 @@ module Dimscan
       end
 
       it 'yields a byte at a time' do
-        HTTPByteEnumerator.instance_variable_set(:@connections, {})
         expect { |b| enumerator.each(&b) }.to yield_successive_args(1, 2, 3, 4)
       end
 
       it 'returns an enumerator that returns the bytes if no block is given' do
-        HTTPByteEnumerator.instance_variable_set(:@connections, {})
         enum = enumerator.each
         expect(enum).to be_an(Enumerator)
         expect(enum.next).to eq(1)
